8252efcea133c208bceec7040a6dd5088918ae0b
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-07-06 11:24:33 +0200

    Allways get the agent.

--- frameworks/Kieker-java/functions.sh
+++ frameworks/Kieker-java/functions.sh
@@ -9,16 +9,14 @@ fi
 
 
 function getAgent() {
-	info "Checking whether Kieker is present in ${AGENT}"
-	if [ ! -f "${AGENT}" ] ; then
-		# get agent
-		export VERSION_PATH=`curl "https://oss.sonatype.org/service/local/repositories/snapshots/content/net/kieker-monitoring/kieker/" | grep '<resourceURI>' | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | grep '/$' | grep -v ".xml" | head -n 1`
-		export AGENT_PATH=`curl "${VERSION_PATH}" | grep 'aspectj.jar</resourceURI' | sort | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | tail -1`
-		curl "${AGENT_PATH}" > "${AGENT}"
+	info "Download the Kieker agent ${AGENT}"
+	# get agent
+	export VERSION_PATH=`curl "https://oss.sonatype.org/service/local/repositories/snapshots/content/net/kieker-monitoring/kieker/" | grep '<resourceURI>' | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | grep '/$' | grep -v ".xml" | head -n 1`
+	export AGENT_PATH=`curl "${VERSION_PATH}" | grep 'aspectj.jar</resourceURI' | sort | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | tail -1`
+	curl "${AGENT_PATH}" > "${AGENT}"
 
-		if [ ! -f "${AGENT}" ] || [ -s "${AGENT}" ] ; then
-			error "Kieker download from $AGENT_PATH failed; please asure that a correct Kieker AspectJ file is present!"
-		fi
+	if [ ! -f "${AGENT}" ] || [ -s "${AGENT}" ] ; then
+		error "Kieker download from $AGENT_PATH failed; please asure that a correct Kieker AspectJ file is present!"
 	fi
 }
 
