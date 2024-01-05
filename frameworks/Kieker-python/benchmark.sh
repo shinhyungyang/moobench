59f09ebecb9d908b7da93613107670dbaa8053e7
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-01-05 17:43:30 +0100

    Use correct python configurations

--- frameworks/Kieker-python/benchmark.sh
+++ frameworks/Kieker-python/benchmark.sh
@@ -49,8 +49,8 @@ fi
 
 if [ -z "$MOOBENCH_CONFIGURATIONS" ]
 then
-	MOOBENCH_CONFIGURATIONS="0 1 3 4"
-	echo "Setting default configuration $MOOBENCH_CONFIGURATIONS (without text logging)"
+	MOOBENCH_CONFIGURATIONS="0 1 2 3 4 5 6 7 8"
+	echo "Setting default configuration $MOOBENCH_CONFIGURATIONS (everything)"
 fi
 echo "Running configurations: $MOOBENCH_CONFIGURATIONS"
 
