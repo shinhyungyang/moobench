deb00f762619cc7a2b54b70e4a6fb35f0e822dcb
DaGeRe <davidgeorg_reichelt@dagere.de>
2024-05-08 15:07:01 +0100

    Manually cleanup data

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -2,105 +2,7 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1715176695136,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
-    "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "davidgeorg_reichelt@dagere.de",
-            "name": "DaGeRe",
-            "username": "DaGeRe"
-          },
-          "committer": {
-            "email": "davidgeorg_reichelt@dagere.de",
-            "name": "DaGeRe",
-            "username": "DaGeRe"
-          },
-          "distinct": true,
-          "id": "2acde057b0332b0673a7246c7190fa0e99e4f3bd",
-          "message": "Remove external file",
-          "timestamp": "2024-05-08T11:47:05+01:00",
-          "tree_id": "238ef2fcf52d1ea824bbd181f1249cc6c55061dd",
-          "url": "https://github.com/kieker-monitoring/moobench/commit/2acde057b0332b0673a7246c7190fa0e99e4f3bd"
-        },
-        "date": 1715165446957,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Configuration 0",
-            "value": 2256.1,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 1",
-            "value": 5781580,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 2",
-            "value": 4990710,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 4",
-            "value": 8257040,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 5",
-            "value": 6518060,
-            "unit": "microseconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "davidgeorg_reichelt@dagere.de",
-            "name": "DaGeRe",
-            "username": "DaGeRe"
-          },
-          "committer": {
-            "email": "davidgeorg_reichelt@dagere.de",
-            "name": "DaGeRe",
-            "username": "DaGeRe"
-          },
-          "distinct": true,
-          "id": "ff700f7047d2dc76a69490a8e1c5c329b30db01b",
-          "message": "Rename MooBench workflow step",
-          "timestamp": "2024-05-08T11:54:44+01:00",
-          "tree_id": "1e62c41f9be32eabcc3a3a99ca89f79f661c4af7",
-          "url": "https://github.com/kieker-monitoring/moobench/commit/ff700f7047d2dc76a69490a8e1c5c329b30db01b"
-        },
-        "date": 1715165902754,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Configuration 0",
-            "value": 2259.3,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 1",
-            "value": 6257400,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 2",
-            "value": 4761460,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 4",
-            "value": 8869730,
-            "unit": "microseconds"
-          },
-          {
-            "name": "Configuration 5",
-            "value": 5919170,
-            "unit": "microseconds"
-          }
-        ]
-      },
+    "Kieker-java": [
       {
         "commit": {
           "author": {
@@ -144,9 +46,7 @@ window.BENCHMARK_DATA = {
             "unit": "microseconds"
           }
         ]
-      }
-    ],
-    "Kieker-java": [
+      },
       {
         "commit": {
           "author": {
