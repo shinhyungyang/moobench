52f6d8ee7b5fbdf8b45f72e0ca24fb9299feeef9
github-action-benchmark <github@users.noreply.github.com>
2024-05-08 10:50:47 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2acde057b0332b0673a7246c7190fa0e99e4f3bd

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1715165447913,
+  "repoUrl": "https://github.com/kieker-monitoring/moobench",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "2acde057b0332b0673a7246c7190fa0e99e4f3bd",
+          "message": "Remove external file",
+          "timestamp": "2024-05-08T11:47:05+01:00",
+          "tree_id": "238ef2fcf52d1ea824bbd181f1249cc6c55061dd",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/2acde057b0332b0673a7246c7190fa0e99e4f3bd"
+        },
+        "date": 1715165446957,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 2256.1,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 1",
+            "value": 5781580,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 2",
+            "value": 4990710,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 4",
+            "value": 8257040,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 5",
+            "value": 6518060,
+            "unit": "microseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
