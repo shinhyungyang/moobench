53625e6e9fbe60646441d900cce6073c10d2cf27
github-action-benchmark <github@users.noreply.github.com>
2024-05-08 13:58:15 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 250d20c6693b3402d435f33394e0583e142c653b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715169036538,
+  "lastUpdate": 1715176695136,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Benchmark": [
@@ -141,6 +141,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Configuration 4",
             "value": 3451.99,
+            "unit": "microseconds"
+          }
+        ]
+      }
+    ],
+    "Kieker-java": [
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "250d20c6693b3402d435f33394e0583e142c653b",
+          "message": "Set Benchmark Name",
+          "timestamp": "2024-05-08T14:08:19+01:00",
+          "tree_id": "269214e6fb02af63c636097ed0cfce1488c929ba",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/250d20c6693b3402d435f33394e0583e142c653b"
+        },
+        "date": 1715176694662,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 90.5236,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 1",
+            "value": 535.877,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 2",
+            "value": 2044.68,
+            "unit": "microseconds"
+          },
+          {
+            "name": "Configuration 4",
+            "value": 3443.47,
             "unit": "microseconds"
           }
         ]
