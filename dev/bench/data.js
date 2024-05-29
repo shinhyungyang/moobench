3142bb2f2a8136a6afc89603ed1f2d92c1575e66
github-action-benchmark <github@users.noreply.github.com>
2024-05-29 04:07:19 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for 499cda753e424def2d900429099fb2af28c48ac2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716955588439,
+  "lastUpdate": 1716955639322,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -5030,6 +5030,52 @@ window.BENCHMARK_DATA = {
             "value": 19345.6,
             "unit": "ns",
             "range": 968.089
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "499cda753e424def2d900429099fb2af28c48ac2",
+          "message": "Also specify distribution",
+          "timestamp": "2024-05-28T09:16:12Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/499cda753e424def2d900429099fb2af28c48ac2"
+        },
+        "date": 1716955638882,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 104.387,
+            "unit": "ns",
+            "range": 0.616684
+          },
+          {
+            "name": "Configuration 1",
+            "value": 863.752,
+            "unit": "ns",
+            "range": 9.67546
+          },
+          {
+            "name": "Configuration 2",
+            "value": 3343.06,
+            "unit": "ns",
+            "range": 154.291
+          },
+          {
+            "name": "Configuration 4",
+            "value": 19070.8,
+            "unit": "ns",
+            "range": 1086.02
           }
         ]
       }
