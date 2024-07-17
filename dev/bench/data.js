159089dc97ad314a5e3fa9955d0b577612be0b24
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 03:52:28 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for 05f37bc2712a998109f2a0077a4a5e5ef955e7d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721106951951,
+  "lastUpdate": 1721188348813,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -8792,6 +8792,46 @@ window.BENCHMARK_DATA = {
             "value": 4801.42,
             "unit": "ns",
             "range": 173.204
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "05f37bc2712a998109f2a0077a4a5e5ef955e7d1",
+          "message": "Also specify java distribution for the python action",
+          "timestamp": "2024-07-15T18:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/05f37bc2712a998109f2a0077a4a5e5ef955e7d1"
+        },
+        "date": 1721188347771,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 90.6675,
+            "unit": "ns",
+            "range": 0.522756
+          },
+          {
+            "name": "Configuration 1",
+            "value": 2923.24,
+            "unit": "ns",
+            "range": 81.1851
+          },
+          {
+            "name": "Configuration 3",
+            "value": 4875.73,
+            "unit": "ns",
+            "range": 88.545
           }
         ]
       }
