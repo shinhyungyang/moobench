5f238e1e37c4ae67253c95df9adfb828c257eeae
github-action-benchmark <github@users.noreply.github.com>
2024-07-30 04:08:51 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for 05f37bc2712a998109f2a0077a4a5e5ef955e7d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722312220481,
+  "lastUpdate": 1722312531805,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -17662,6 +17662,52 @@ window.BENCHMARK_DATA = {
             "value": 18483.7,
             "unit": "ns",
             "range": 1021.31
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "05f37bc2712a998109f2a0077a4a5e5ef955e7d1",
+          "message": "Also specify java distribution for the python action",
+          "timestamp": "2024-07-15T18:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/05f37bc2712a998109f2a0077a4a5e5ef955e7d1"
+        },
+        "date": 1722312531150,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 104.041,
+            "unit": "ns",
+            "range": 0.350175
+          },
+          {
+            "name": "Configuration 1",
+            "value": 859.206,
+            "unit": "ns",
+            "range": 14.6238
+          },
+          {
+            "name": "Configuration 2",
+            "value": 3318.98,
+            "unit": "ns",
+            "range": 139.418
+          },
+          {
+            "name": "Configuration 4",
+            "value": 18865.8,
+            "unit": "ns",
+            "range": 1048.38
           }
         ]
       }
