690e091535b2013b5cf206530d1bbf519947c86c
github-action-benchmark <github@users.noreply.github.com>
2024-07-31 04:09:12 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for 05f37bc2712a998109f2a0077a4a5e5ef955e7d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722398773236,
+  "lastUpdate": 1722398952471,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -17840,6 +17840,52 @@ window.BENCHMARK_DATA = {
             "value": 18865.8,
             "unit": "ns",
             "range": 1048.38
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "05f37bc2712a998109f2a0077a4a5e5ef955e7d1",
+          "message": "Also specify java distribution for the python action",
+          "timestamp": "2024-07-15T18:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/05f37bc2712a998109f2a0077a4a5e5ef955e7d1"
+        },
+        "date": 1722398951718,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 104.272,
+            "unit": "ns",
+            "range": 0.821175
+          },
+          {
+            "name": "Configuration 1",
+            "value": 877.394,
+            "unit": "ns",
+            "range": 14.8229
+          },
+          {
+            "name": "Configuration 2",
+            "value": 3303.57,
+            "unit": "ns",
+            "range": 119.195
+          },
+          {
+            "name": "Configuration 4",
+            "value": 18074.5,
+            "unit": "ns",
+            "range": 202.66
           }
         ]
       }
