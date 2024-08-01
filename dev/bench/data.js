df06dfb9fcf4aced69aee1a2dfc2f187eec55c68
github-action-benchmark <github@users.noreply.github.com>
2024-08-01 04:02:02 +0000

    add Kieker-java-2 (customSmallerIsBetter) benchmark result for 05f37bc2712a998109f2a0077a4a5e5ef955e7d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722484303553,
+  "lastUpdate": 1722484922766,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -13982,6 +13982,52 @@ window.BENCHMARK_DATA = {
             "value": 6250.16,
             "unit": "ns",
             "range": 568.171
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "05f37bc2712a998109f2a0077a4a5e5ef955e7d1",
+          "message": "Also specify java distribution for the python action",
+          "timestamp": "2024-07-15T18:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/05f37bc2712a998109f2a0077a4a5e5ef955e7d1"
+        },
+        "date": 1722484922056,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 92.1904,
+            "unit": "ns",
+            "range": 0.312746
+          },
+          {
+            "name": "Configuration 1",
+            "value": 561.702,
+            "unit": "ns",
+            "range": 15.4415
+          },
+          {
+            "name": "Configuration 2",
+            "value": 2541.16,
+            "unit": "ns",
+            "range": 81.1054
+          },
+          {
+            "name": "Configuration 4",
+            "value": 6659.63,
+            "unit": "ns",
+            "range": 253.108
           }
         ]
       }
