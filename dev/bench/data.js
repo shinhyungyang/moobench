90245add55c6df341b6364add2103d50053cef10
github-action-benchmark <github@users.noreply.github.com>
2024-08-02 04:07:56 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for 05f37bc2712a998109f2a0077a4a5e5ef955e7d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722571307965,
+  "lastUpdate": 1722571676736,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -18150,6 +18150,52 @@ window.BENCHMARK_DATA = {
             "value": 18231.6,
             "unit": "ns",
             "range": 708.526
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "05f37bc2712a998109f2a0077a4a5e5ef955e7d1",
+          "message": "Also specify java distribution for the python action",
+          "timestamp": "2024-07-15T18:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/05f37bc2712a998109f2a0077a4a5e5ef955e7d1"
+        },
+        "date": 1722571675454,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 104.091,
+            "unit": "ns",
+            "range": 0.579315
+          },
+          {
+            "name": "Configuration 1",
+            "value": 831.494,
+            "unit": "ns",
+            "range": 29.3445
+          },
+          {
+            "name": "Configuration 2",
+            "value": 3330.36,
+            "unit": "ns",
+            "range": 135.657
+          },
+          {
+            "name": "Configuration 4",
+            "value": 18397.5,
+            "unit": "ns",
+            "range": 707.793
           }
         ]
       }
