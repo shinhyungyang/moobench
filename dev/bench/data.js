d78d4d67c32bf1cb24bfe447383053fa512e6033
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 03:52:28 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for 514d24bde397a182d2dfea3df1e95841e482e34c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724647004689,
+  "lastUpdate": 1724730748312,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -12904,6 +12904,46 @@ window.BENCHMARK_DATA = {
             "value": 5009.04,
             "unit": "ns",
             "range": 180.973
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "514d24bde397a182d2dfea3df1e95841e482e34c",
+          "message": "Update wording: MooBench measures all overhead of all pillars of observability",
+          "timestamp": "2024-08-22T09:13:18Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/514d24bde397a182d2dfea3df1e95841e482e34c"
+        },
+        "date": 1724730746778,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Configuration 0",
+            "value": 90.9056,
+            "unit": "ns",
+            "range": 1.07523
+          },
+          {
+            "name": "Configuration 1",
+            "value": 2904.89,
+            "unit": "ns",
+            "range": 104.31
+          },
+          {
+            "name": "Configuration 3",
+            "value": 4802.81,
+            "unit": "ns",
+            "range": 126.879
           }
         ]
       }
