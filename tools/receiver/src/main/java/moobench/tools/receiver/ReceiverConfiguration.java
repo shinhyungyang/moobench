42aee1408cd17cf547e63db254acaa1e88ec6226
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-10-17 22:22:03 +0200

    Update to Kieker 2.0.0 in receiver

--- tools/receiver/src/main/java/moobench/tools/receiver/ReceiverConfiguration.java
+++ tools/receiver/src/main/java/moobench/tools/receiver/ReceiverConfiguration.java
@@ -1,15 +1,17 @@
 package moobench.tools.receiver;
 
-import kieker.analysis.source.rewriter.NoneTraceMetadataRewriter;
-import kieker.analysis.source.tcp.MultipleConnectionTcpSourceStage;
-import kieker.analysis.stage.forward.CountingFilter;
+import kieker.analysis.generic.CountingStage;
+import kieker.analysis.generic.source.rewriter.NoneTraceMetadataRewriter;
+import kieker.analysis.generic.source.tcp.MultipleConnectionTcpSourceStage;
+import kieker.common.record.IMonitoringRecord;
+
 import teetime.framework.Configuration;
 
 public class ReceiverConfiguration extends Configuration {
 
 	public ReceiverConfiguration(final int inputPort, final int bufferSize) {
 		MultipleConnectionTcpSourceStage source = new MultipleConnectionTcpSourceStage(inputPort, bufferSize, new NoneTraceMetadataRewriter());
-		CountingFilter counting = new CountingFilter();
+		CountingStage<IMonitoringRecord> counting = new CountingStage<>(false, Integer.MAX_VALUE);
 		
 		connectPorts(source.getOutputPort(), counting.getInputPort());
 	}
