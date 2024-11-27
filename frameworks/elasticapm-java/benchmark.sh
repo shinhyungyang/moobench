7c68370393c1109a43534dcac064a383b92d521b
DaGeRe <davidgeorg_reichelt@dagere.de>
2024-11-27 18:10:48 +0100

    Start working on elastic apm

---
+++ frameworks/elasticapm-java/benchmark.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+#
+# Kieker benchmark script
+#
+# Usage: benchmark.sh
+
+# configure base dir
+BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+MAIN_DIR="${BASE_DIR}/../.."
+
+# Hotfix for ASPECTJ
+# https://stackoverflow.com/questions/70411097/instrument-java-17-with-aspectj
+JAVA_VERSION=$(java -version 2>&1 | grep version | sed 's/^.* "\([0-9\.]*\).*/\1/g')
+if [ "${JAVA_VERSION}" != "1.8.0" ] ; then
+	export JAVA_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED"
+	echo "Setting \$JAVA_OPTS, since Java version is bigger than 8"
+fi
+
+#
+# source functionality
+#
+
+if [ ! -d "${BASE_DIR}" ] ; then
+	echo "Base directory ${BASE_DIR} does not exist."
+	exit 1
+fi
+
+if [ -f "${MAIN_DIR}/common-functions.sh" ] ; then
+	source "${MAIN_DIR}/common-functions.sh"
+else
+	echo "Missing library: ${MAIN_DIR}/common-functions.sh"
+	exit 1
+fi
+
+# load configuration and common functions
+if [ -f "${BASE_DIR}/config.rc" ] ; then
+	source "${BASE_DIR}/config.rc"
+else
+	echo "Missing configuration: ${BASE_DIR}/config.rc"
+	exit 1
+fi
+
+if [ -f "${BASE_DIR}/functions.sh" ] ; then
+	source "${BASE_DIR}/functions.sh"
+else
+	echo "Missing: ${BASE_DIR}/functions.sh"
+	exit 1
+fi
+if [ -f "${BASE_DIR}/labels.sh" ] ; then
+	source "${BASE_DIR}/labels.sh"
+else
+	echo "Missing file: ${BASE_DIR}/labels.sh"
+	exit 1
+fi
+
+if [ -z "$MOOBENCH_CONFIGURATIONS" ]
+then
+	MOOBENCH_CONFIGURATIONS="0 1 2 4 5"
+	echo "Setting default configuration $MOOBENCH_CONFIGURATIONS (without TextLogStreamHandler)"
+fi
+echo "Running configurations: $MOOBENCH_CONFIGURATIONS"
+
+#
+# Setup
+#
+
+info "----------------------------------"
+info "Setup..."
+info "----------------------------------"
+
+cd "${BASE_DIR}"
+
+# load agent
+getAgent
+
+checkDirectory data-dir "${DATA_DIR}" create
+checkFile log "${DATA_DIR}/kieker.log" clean
+cleanupResults
+mkdir -p $RESULTS_DIR
+PARENT=`dirname "${RESULTS_DIR}"`
+checkDirectory result-base "${PARENT}"
+
+checkFile AspectJ-Agent "${AGENT}"
+
+checkExecutable java "${JAVA_BIN}"
+checkExecutable moobench "${MOOBENCH_BIN}"
+checkFile R-script "${RSCRIPT_PATH}"
+
+showParameter
+
+TIME=`expr ${METHOD_TIME} \* ${TOTAL_NUM_OF_CALLS} / 1000000000 \* 4 \* ${RECURSION_DEPTH} \* ${NUM_OF_LOOPS} + ${SLEEP_TIME} \* 4 \* ${NUM_OF_LOOPS}  \* ${RECURSION_DEPTH} + 50 \* ${TOTAL_NUM_OF_CALLS} / 1000000000 \* 4 \* ${RECURSION_DEPTH} \* ${NUM_OF_LOOPS} `
+info "Experiment will take circa ${TIME} seconds."
+
+# general server arguments
+JAVA_ARGS="-Xms1G -Xmx2G"
+
+LTW_ARGS="-javaagent:elastic-apm-agent.jar"
+
+KIEKER_ARGS="-Dlog4j.configuration=log4j.cfg -Dkieker.monitoring.name=KIEKER-BENCHMARK -Dkieker.monitoring.adaptiveMonitoring.enabled=false -Dkieker.monitoring.periodicSensorsExecutorPoolSize=0"
+
+# JAVA_ARGS used to configure and setup a specific writer
+declare -a WRITER_CONFIG
+# Receiver setup if necessary
+declare -a RECEIVER
+# Title
+declare -a TITLE
+
+#
+# Different writer setups
+#
+WRITER_CONFIG[0]=""
+WRITER_CONFIG[1]="-Delastic.apm.service_name=moobench-benchmark -Delastic.apm.application_packages=moobench.application -Delastic.apm.server_url=http://127.0.0.1:8200"
+
+
+
+writeConfiguration
+checkMoobenchConfiguration
+#
+# Run benchmark
+#
+
+info "----------------------------------"
+info "Running benchmark..."
+info "----------------------------------"
+
+for ((i=1;i<=${NUM_OF_LOOPS};i+=1))
+do
+    info "## Starting iteration ${i}/${NUM_OF_LOOPS}"
+    echo "## Starting iteration ${i}/${NUM_OF_LOOPS}" >> "${DATA_DIR}/kieker.log"
+
+    executeBenchmark
+    printIntermediaryResults "${i}"
+done
+
+# Create R labels
+LABELS=$(createRLabels)
+runStatistics
+
+cleanupResults
+
+mv "${DATA_DIR}/kieker.log" "${RESULTS_DIR}/kieker.log"
+[ -f "${RESULTS_DIR}/hotspot-1-${RECURSION_DEPTH}-1.log" ] && grep "<task " "${RESULTS_DIR}/"hotspot-*.log > "${RESULTS_DIR}/java.log"
+[ -f "${DATA_DIR}/errorlog.txt" ] && mv "${DATA_DIR}/errorlog.txt" "${RESULTS_DIR}"
+
+checkFile results.yaml "${RESULTS_DIR}/results.yaml"
+checkFile results.yaml "${RESULTS_DIR}/results.zip"
+
+info "Done."
+
+exit 0
+# end
---
+++ frameworks/elasticapm-java/functions.sh
@@ -0,0 +1,107 @@
+# Kieker specific functions
+
+# ensure the script is sourced
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "Hey, you should source this script, not execute it!"
+    exit 1
+fi
+
+
+function getAgent() {
+	info "Download the elastic agent ${AGENT}"
+	# get agent
+	curl -o 'elastic-apm-agent.jar' -L 'https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=co.elastic.apm&a=elastic-apm-agent&v=LATEST'
+}
+
+# experiment setups
+
+#################################
+# function: execute an experiment
+#
+# $1 = i iterator
+# $2 = j iterator
+# $3 = k iterator
+# $4 = title
+# $5 = writer parameters
+function executeExperiment() {
+    loop="$1"
+    recursion="$2"
+    index="$3"
+    title="${TITLE[$index]}"
+    kieker_parameters="${WRITER_CONFIG[$index]}"
+
+    info " # ${loop}.${recursion}.${index} ${title}"
+    echo " # ${loop}.${recursion}.${index} ${title}" >> "${DATA_DIR}/kieker.log"
+
+    if [  "${kieker_parameters}" == "" ] ; then
+       export BENCHMARK_OPTS="${JAVA_ARGS}"
+    else
+       export BENCHMARK_OPTS="${JAVA_ARGS} ${LTW_ARGS} ${KIEKER_ARGS} ${kieker_parameters}"
+    fi
+
+    debug "Run options: ${BENCHMARK_OPTS}"
+
+    RESULT_FILE="${RAWFN}-${loop}-${recursion}-${index}.csv"
+    LOG_FILE="${RESULTS_DIR}/output_${loop}_${RECURSION_DEPTH}_${index}.txt"
+
+    "${MOOBENCH_BIN}" \
+	--application moobench.application.MonitoredClassSimple \
+        --output-filename "${RESULT_FILE}" \
+        --total-calls "${TOTAL_NUM_OF_CALLS}" \
+        --method-time "${METHOD_TIME}" \
+        --total-threads $THREADS \
+        --recursion-depth "${recursion}"
+
+    if [ ! -f "${RESULT_FILE}" ] ; then
+        info "---------------------------------------------------"
+        cat "${LOG_FILE}"
+        error "Result file '${RESULT_FILE}' is empty."
+    else
+       size=`wc -c "${RESULT_FILE}" | awk '{ print $1 }'`
+       if [ "${size}" == "0" ] ; then
+           info "---------------------------------------------------"
+           cat "${LOG_FILE}"
+           error "Result file '${RESULT_FILE}' is empty."
+       fi
+    fi
+    rm -rf "${DATA_DIR}"/kieker-*
+
+    [ -f "${DATA_DIR}/hotspot.log" ] && mv "${DATA_DIR}/hotspot.log" "${RESULTS_DIR}/hotspot-${loop}-${recursion}-${index}.log"
+    echo >> "${DATA_DIR}/kieker.log"
+    echo >> "${DATA_DIR}/kieker.log"
+    sync
+    sleep "${SLEEP_TIME}"
+}
+
+function executeBenchmarkBody() {
+  index="$1"
+  loop="$2"
+  recursion="$3"
+  if [[ "${RECEIVER[$index]}" ]] ; then
+     debug "receiver ${RECEIVER[$index]}"
+     ${RECEIVER[$index]} >> "${DATA_DIR}/kieker.receiver-${loop}-${index}.log" &
+     RECEIVER_PID=$!
+     debug "PID ${RECEIVER_PID}"
+  fi
+
+  executeExperiment "$loop" "$recursion" "$index"
+
+  if [[ "${RECEIVER_PID}" ]] ; then
+     kill -TERM "${RECEIVER_PID}"
+     unset RECEIVER_PID
+  fi
+}
+
+## Execute Benchmark
+function executeBenchmark() {
+    recursion="${RECURSION_DEPTH}"
+
+    for index in $MOOBENCH_CONFIGURATIONS
+    do
+      executeBenchmarkBody $index $i $recursion
+    done
+}
+
+
+# end
