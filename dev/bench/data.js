bfd45a1d2faaf72f37b791e60e1bbf709b8567c9
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 04:25:47 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for e492ed02745ea03a8ea5dc26b472d41ae271fdc9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733458959159,
+  "lastUpdate": 1733459147186,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -44086,6 +44086,52 @@ window.BENCHMARK_DATA = {
             "value": 18048.7,
             "unit": "ns",
             "range": 1424.22
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "e492ed02745ea03a8ea5dc26b472d41ae271fdc9",
+          "message": "Added fixes for MooBench table style\n\nIt adds two classes ``moobench-table-label`` and ``moobench-table-value``.\nThe added classes will look for the appropriate style in style.css.  The\noriginal WordPress theme for kieker-monitoring.net is uploaded on the\nnew https://github.com/shinhyungyang/wp-theme-kieker repository, and the\nfixes for the MooBench table style is committed as the\n``fix-moobench-table-style`` branch on the repository.",
+          "timestamp": "2024-12-04T15:06:51Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/e492ed02745ea03a8ea5dc26b472d41ae271fdc9"
+        },
+        "date": 1733459145145,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 104.237,
+            "unit": "ns",
+            "range": 0.406384
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 878.127,
+            "unit": "ns",
+            "range": 13.1927
+          },
+          {
+            "name": "No logging",
+            "value": 3287.06,
+            "unit": "ns",
+            "range": 167.385
+          },
+          {
+            "name": "Binary file",
+            "value": 17669.1,
+            "unit": "ns",
+            "range": 1080.37
           }
         ]
       }
