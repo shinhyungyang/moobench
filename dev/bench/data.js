07d533982395bd2247c6291681ea62a399de89bf
github-action-benchmark <github@users.noreply.github.com>
2024-12-08 04:30:45 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for e492ed02745ea03a8ea5dc26b472d41ae271fdc9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733632216712,
+  "lastUpdate": 1733632245591,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -22968,6 +22968,52 @@ window.BENCHMARK_DATA = {
             "value": 4571.44,
             "unit": "ns",
             "range": 78.4051
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "e492ed02745ea03a8ea5dc26b472d41ae271fdc9",
+          "message": "Added fixes for MooBench table style\n\nIt adds two classes ``moobench-table-label`` and ``moobench-table-value``.\nThe added classes will look for the appropriate style in style.css.  The\noriginal WordPress theme for kieker-monitoring.net is uploaded on the\nnew https://github.com/shinhyungyang/wp-theme-kieker repository, and the\nfixes for the MooBench table style is committed as the\n``fix-moobench-table-style`` branch on the repository.",
+          "timestamp": "2024-12-04T15:06:51Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/e492ed02745ea03a8ea5dc26b472d41ae271fdc9"
+        },
+        "date": 1733632243723,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.6555,
+            "unit": "ns",
+            "range": 0.208336
+          },
+          {
+            "name": "No logging",
+            "value": 2937.99,
+            "unit": "ns",
+            "range": 88.4133
+          },
+          {
+            "name": "Zipkin",
+            "value": 5011.41,
+            "unit": "ns",
+            "range": 148.892
+          },
+          {
+            "name": "Prometheus",
+            "value": 4591.25,
+            "unit": "ns",
+            "range": 112.461
           }
         ]
       }
