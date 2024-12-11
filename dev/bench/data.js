3bcf17243087a3f860ce03a34604e4e3f47985d6
github-action-benchmark <github@users.noreply.github.com>
2024-12-11 04:26:57 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for e492ed02745ea03a8ea5dc26b472d41ae271fdc9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733891144649,
+  "lastUpdate": 1733891217379,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -60106,6 +60106,52 @@ window.BENCHMARK_DATA = {
             "value": 7172.82,
             "unit": "ns",
             "range": 840.979
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "e492ed02745ea03a8ea5dc26b472d41ae271fdc9",
+          "message": "Added fixes for MooBench table style\n\nIt adds two classes ``moobench-table-label`` and ``moobench-table-value``.\nThe added classes will look for the appropriate style in style.css.  The\noriginal WordPress theme for kieker-monitoring.net is uploaded on the\nnew https://github.com/shinhyungyang/wp-theme-kieker repository, and the\nfixes for the MooBench table style is committed as the\n``fix-moobench-table-style`` branch on the repository.",
+          "timestamp": "2024-12-04T15:06:51Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/e492ed02745ea03a8ea5dc26b472d41ae271fdc9"
+        },
+        "date": 1733891215436,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8868,
+            "unit": "ns",
+            "range": 0.125703
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 322.996,
+            "unit": "ns",
+            "range": 15.062
+          },
+          {
+            "name": "Regular Writing",
+            "value": 7595.24,
+            "unit": "ns",
+            "range": 873.537
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8250.98,
+            "unit": "ns",
+            "range": 1125.21
           }
         ]
       }
