7da0460bd7fd79238b76447df98eb21f08b28bc0
github-action-benchmark <github@users.noreply.github.com>
2024-12-13 11:50:51 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 910edec180c53c34a77498c9c21a102e254d5361

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734065681756,
+  "lastUpdate": 1734090651156,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -13022,6 +13022,54 @@ window.BENCHMARK_DATA = {
             "value": 3119.74,
             "unit": "ns",
             "range": 59.9371
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "910edec180c53c34a77498c9c21a102e254d5361",
+          "message": "Remove duplicated service name",
+          "timestamp": "2024-12-13T12:03:09+01:00",
+          "tree_id": "5b5b80ccb9b18b7dd2076da20dd3e58a56f7b8ec",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/910edec180c53c34a77498c9c21a102e254d5361"
+        },
+        "date": 1734090647628,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.7691,
+            "unit": "ns",
+            "range": 0.186217
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 438.244,
+            "unit": "ns",
+            "range": 3.82667
+          },
+          {
+            "name": "No logging",
+            "value": 1839.27,
+            "unit": "ns",
+            "range": 8.9952
+          },
+          {
+            "name": "Binary file",
+            "value": 3181.7,
+            "unit": "ns",
+            "range": 83.5378
           }
         ]
       }
