f6b1e9cb6e7fa748a0621057349cf96cd12369ef
github-action-benchmark <github@users.noreply.github.com>
2024-12-25 04:05:01 +0000

    add Kieker-java-2 (customSmallerIsBetter) benchmark result for 910edec180c53c34a77498c9c21a102e254d5361

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735015134247,
+  "lastUpdate": 1735099501193,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -36156,6 +36156,52 @@ window.BENCHMARK_DATA = {
             "value": 6276.96,
             "unit": "ns",
             "range": 508.325
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "910edec180c53c34a77498c9c21a102e254d5361",
+          "message": "Remove duplicated service name",
+          "timestamp": "2024-12-13T11:02:12Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/910edec180c53c34a77498c9c21a102e254d5361"
+        },
+        "date": 1735099498610,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 91.9587,
+            "unit": "ns",
+            "range": 0.355432
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 553.406,
+            "unit": "ns",
+            "range": 11.6326
+          },
+          {
+            "name": "No logging",
+            "value": 2513.91,
+            "unit": "ns",
+            "range": 82.8452
+          },
+          {
+            "name": "Binary file",
+            "value": 6162.67,
+            "unit": "ns",
+            "range": 667.417
           }
         ]
       }
