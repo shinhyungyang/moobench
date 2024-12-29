ca36e84e8256768a5b179518c0e0ec5860e39c22
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-12-29 23:17:38 +0100

    Start pinpoint implementation (not working yet)

---
+++ frameworks/pinpoint-java/benchmark.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+#
+# OpenTelemetry benchmark script
+#
+# Usage: benchmark.sh
+
+# configure base dir
+BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+
+#
+# source functionality
+#
+
+if [ ! -d "${BASE_DIR}" ] ; then
+	echo "Base directory ${BASE_DIR} does not exist."
+	exit 1
+fi
+
+MAIN_DIR="${BASE_DIR}/../.."
+
+if [ -f "${MAIN_DIR}/common-functions.sh" ] ; then
+	source "${MAIN_DIR}/common-functions.sh"
+else
+	echo "Missing library: ${MAIN_DIR}/common-functions.sh"
+	exit 1
+fi
+
+# load configuration and common functions
+if [ -f "${BASE_DIR}/config.rc" ] ; then
+	source "${BASE_DIR}/config.rc"
+else
+	echo "Missing configuration: ${BASE_DIR}/config.rc"
+	exit 1
+fi
+
+if [ -f "${BASE_DIR}/functions.sh" ] ; then
+	source "${BASE_DIR}/functions.sh"
+else
+	echo "Missing: ${BASE_DIR}/functions.sh"
+	exit 1
+fi
+if [ -f "${BASE_DIR}/labels.sh" ] ; then
+	source "${BASE_DIR}/labels.sh"
+else
+	echo "Missing file: ${BASE_DIR}/labels.sh"
+	exit 1
+fi
+
+if [ -z "$MOOBENCH_CONFIGURATIONS" ]
+then
+	MOOBENCH_CONFIGURATIONS="0 1 3"
+	echo "Setting default configuration $MOOBENCH_CONFIGURATIONS (without text logging)"
+fi
+echo "Running configurations: $MOOBENCH_CONFIGURATIONS"
+
+#
+# Setup
+#
+
+info "----------------------------------"
+info "Setup..."
+info "----------------------------------"
+
+# load agent
+getAgent
+
+checkFile log "${BASE_DIR}/OpenTelemetry.log" clean
+checkDirectory results-directory "${RESULTS_DIR}" recreate
+
+checkFile opentelemetry-agent "${AGENT_JAR}"
+
+checkExecutable java "${JAVA_BIN}"
+checkExecutable moobench "${MOOBENCH_BIN}"
+checkFile R-script "${RSCRIPT_PATH}"
+
+showParameter
+
+TIME=`expr ${METHOD_TIME} \* ${TOTAL_NUM_OF_CALLS} / 1000000000 \* 4 \* ${RECURSION_DEPTH} \* ${NUM_OF_LOOPS} + ${SLEEP_TIME} \* 4 \* ${NUM_OF_LOOPS}  \* ${RECURSION_DEPTH} + 50 \* ${TOTAL_NUM_OF_CALLS} / 1000000000 \* 4 \* ${RECURSION_DEPTH} \* ${NUM_OF_LOOPS} `
+info "Experiment will take circa ${TIME} seconds."
+
+# general server arguments
+JAVA_ARGS="-Xms1G -Xmx2G"
+
+JAVA_ARGS_NOINSTR="${JAVA_ARGS}"
+JAVA_ARGS_PINTPOINT_BASIC="${JAVA_ARGS} -javaagent:pinpoint-agent-3.0.1/pinpoint-bootstrap.jar -Dpinpoint.agentId=moobench-agent -Dpinpoint.applicationName=MOOBENCH"
+
+writeConfiguration
+checkMoobenchConfiguration
+
+#
+# Run benchmark
+#
+
+info "----------------------------------"
+info "Running benchmark..."
+info "----------------------------------"
+
+## Execute Benchmark
+for ((i=1;i<=${NUM_OF_LOOPS};i+=1)); do
+    k=0
+    info "## Starting iteration ${i}/${NUM_OF_LOOPS}"
+    echo "## Starting iteration ${i}/${NUM_OF_LOOPS}" >> "${BASE_DIR}/OpenTelemetry.log"
+
+    executeBenchmark
+    printIntermediaryResults "${i}"
+done
+
+# Create R labels
+LABELS=$(createRLabels)
+runStatistics
+
+cleanupResults
+
+mv "${BASE_DIR}/OpenTelemetry.log" "${RESULTS_DIR}/OpenTelemetry.log"
+[ -f "${RESULTS_DIR}/hotspot-1-${RECURSION_DEPTH}-1.log" ] && grep "<task " "${RESULTS_DIR}/"hotspot-*.log > "${RESULTS_DIR}/java.log"
+[ -f "${BASE_DIR}/errorlog.txt" ] && mv "${BASE_DIR}/errorlog.txt" "${RESULTS_DIR}"
+
+checkFile results.yaml "${RESULTS_DIR}/results.yaml"
+checkFile results.yaml "${RESULTS_DIR}/results.zip"
+
+info "Done."
+
+exit 0
+# end
---
+++ frameworks/pinpoint-java/functions.sh
@@ -0,0 +1,147 @@
+# OpenTelemetry specific functions
+
+# ensure the script is sourced
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "Hey, you should source this script, not execute it!"
+    exit 1
+fi
+
+
+function getAgent() {
+    if [ ! -d "${BASE_DIR}/pinpoint" ]
+    then
+        mkdir -p "${BASE_DIR}/pinpoint"
+        cd "${BASE_DIR}/pinpoint"
+        wget --output-document=pinpoint.tar.gz \
+            https://repo1.maven.org/maven2/com/navercorp/pinpoint/pinpoint-agent/3.0.1/pinpoint-agent-3.0.1.tar.gz
+        tar -xvf pinpoint.tar.gz
+        cd $BASE_DIR
+    fi
+}
+
+function getHBase() {
+   if [ ! -d $BASE_DIR/hbase ]
+   then
+      mkdir -p $BASE_DIR/hbase
+      wget https://dlcdn.apache.org/hbase/2.6.1/hbase-2.6.1-bin.tar.gz
+      tar -xvf hbase-2.6.1-bin.tar.gz
+      cd hbase-2.6.1
+      wget https://raw.githubusercontent.com/pinpoint-apm/pinpoint/refs/heads/master/hbase/scripts/hbase-create.hbase
+      cd $BASE_DIR
+   fi
+}
+
+function startHBase() {
+   cd hbase/hbase-2.6.1
+   
+   wget https://raw.githubusercontent.com/pinpoint-apm/pinpoint/refs/heads/master/hbase/scripts/hbase-create.hbase
+   
+   bin/hbase shell hbase-create.hbase
+   bin/start-hbase.sh
+   
+}
+
+function startPinot() {
+	PINOT_VERSION=1.1.0 #set to the Pinot version you decide to use
+	if [ ! -d apache-pinot-$PINOT_VERSION-bin ]
+	then
+		wget https://downloads.apache.org/pinot/apache-pinot-$PINOT_VERSION/apache-pinot-$PINOT_VERSION-bin.tar.gz
+		tar -zxvf apache-pinot-$PINOT_VERSION-bin.tar.gz
+	fi
+	
+	cd apache-pinot-$PINOT_VERSION-bin
+	./bin/pinot-admin.sh QuickStart -type batch &> ${BASE_DIR}/logs/pinot.log
+}
+
+function startKafka() {
+   if [ ! -d kafka_2.13-3.9.0 ]
+   then
+   	wget https://dlcdn.apache.org/kafka/3.9.0/kafka_2.13-3.9.0.tgz
+   	tar -xvf kafka_2.13-3.9.0.tgz
+   fi
+   cd kafka_2.13-3.9.0
+
+   KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
+   bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c config/kraft/reconfig-server.properties
+   bin/kafka-server-start.sh config/kraft/reconfig-server.properties &> ${BASE_DIR}/logs/kafka.log &
+   
+   echo "Waiting for Kafka start..."
+   sleep 3
+   bin/kafka-topics.sh --create --topic inspector-stat-agent-00 --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+   bin/kafka-topics.sh --create --topic inspector-stat-app --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+}
+
+function startCollectorAndWeb() {
+   PINPOINT_VERSION=3.0.1
+
+   if [ ! -f pinpoint-collector-starter-${PINPOINT_VERSION}-exec.jar ]
+   then
+      wget
+   fi
+   
+   java -jar -Dpinpoint.zookeeper.address=localhost pinpoint-collector-starter-${PINPOINT_VERSION}-exec.jar &> ${BASE_DIR}/collector.log
+   
+   if [ ! -f pinpoint-web-starter-${PINPOINT_VERSION}-exec.jar ]
+   then
+      wget https://repo1.maven.org/maven2/com/navercorp/pinpoint/pinpoint-web-starter/3.0.1/pinpoint-web-starter-${PINPOINT_VERSION}-exec.jar
+   fi
+   
+   java -jar pinpoint-web-starter-${PINPOINT_VERSION}-exec.jar &> ${BASE_DIR}/web-starter.log
+   
+   
+}
+
+function cleanup {
+    [ -f "${BASE_DIR}/hotspot.log" ] && mv "${BASE_DIR}/hotspot.log" "${RESULTS_DIR}/hotspot-${i}-$RECURSION_DEPTH-${k}.log"
+    echo >> "${BASE_DIR}/OpenTelemetry.log"
+    echo >> "${BASE_DIR}/OpenTelemetry.log"
+    sync
+    sleep "${SLEEP_TIME}"
+}
+
+## Execute Benchmark
+function executeBenchmark() {
+   for index in $MOOBENCH_CONFIGURATIONS
+   do
+      case $index in
+         0) runNoInstrumentation 0 ;;
+         1) runPinpointBasic 1 ;;
+      esac
+      
+      cleanup
+   done
+}
+
+
+# experiment setups
+
+function runNoInstrumentation {
+    # No instrumentation
+    info " # ${i}.$RECURSION_DEPTH.${k} ${TITLE[$k]}"
+    echo " # ${i}.$RECURSION_DEPTH.${k} ${TITLE[$k]}" >> "${BASE_DIR}/OpenTelemetry.log"
+    export BENCHMARK_OPTS="${JAVA_ARGS_NOINSTR}"
+    "${MOOBENCH_BIN}" --output-filename "${RAWFN}-${i}-$RECURSION_DEPTH-${k}.csv" \
+        --total-calls "${TOTAL_NUM_OF_CALLS}" \
+        --method-time "${METHOD_TIME}" \
+        --total-threads "${THREADS}" \
+        --recursion-depth "${RECURSION_DEPTH}" \
+        ${MORE_PARAMS} &> "${RESULTS_DIR}/output_${i}_${RECURSION_DEPTH}_${k}.txt"
+}
+
+function runPinpointBasic {
+    # OpenTelemetry Instrumentation Logging Deactivated
+    k=$1
+    info " # ${i}.$RECURSION_DEPTH.${k} "${TITLE[$k]}
+    echo " # ${i}.$RECURSION_DEPTH.${k} "${TITLE[$k]} >> "${BASE_DIR}/pinpoint.log"
+    export BENCHMARK_OPTS="${JAVA_ARGS_PINTPOINT_BASIC}"
+    "${MOOBENCH_BIN}" --output-filename "${RAWFN}-${i}-$RECURSION_DEPTH-${k}.csv" \
+        --total-calls "${TOTAL_NUM_OF_CALLS}" \
+        --method-time "${METHOD_TIME}" \
+        --total-threads "${THREADS}" \
+        --recursion-depth "${RECURSION_DEPTH}" \
+        ${MORE_PARAMS} &> "${RESULTS_DIR}/output_${i}_${RECURSION_DEPTH}_${k}.txt"
+}
+
+
+# end
