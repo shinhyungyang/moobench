418b15432f706114ac772fcce499c8736b5072a1
github-action-benchmark <github@users.noreply.github.com>
2024-12-30 04:16:38 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 910edec180c53c34a77498c9c21a102e254d5361

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735532150320,
+  "lastUpdate": 1735532198723,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -65684,6 +65684,52 @@ window.BENCHMARK_DATA = {
             "value": 7701.32,
             "unit": "ns",
             "range": 1148.79
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "910edec180c53c34a77498c9c21a102e254d5361",
+          "message": "Remove duplicated service name",
+          "timestamp": "2024-12-13T11:02:12Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/910edec180c53c34a77498c9c21a102e254d5361"
+        },
+        "date": 1735532196532,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.746,
+            "unit": "ns",
+            "range": 0.15737
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 317.064,
+            "unit": "ns",
+            "range": 16.5427
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8086.01,
+            "unit": "ns",
+            "range": 1207.84
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 7791.35,
+            "unit": "ns",
+            "range": 847.148
           }
         ]
       }
