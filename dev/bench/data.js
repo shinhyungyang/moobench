58e477cbf8993b7a3411119172e9d0d6d083e990
github-action-benchmark <github@users.noreply.github.com>
2025-01-03 18:47:24 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for 8c967add3497418ae20433f8c6ccf5a1077dd0d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735929765873,
+  "lastUpdate": 1735930044478,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -28286,6 +28286,54 @@ window.BENCHMARK_DATA = {
             "value": 4398.13,
             "unit": "ns",
             "range": 130.558
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "8c967add3497418ae20433f8c6ccf5a1077dd0d6",
+          "message": "Get java bin in experiment.rc",
+          "timestamp": "2025-01-03T17:01:51+01:00",
+          "tree_id": "d4da562a2d15f14b2c9f82b71a272b66de3f87de",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/8c967add3497418ae20433f8c6ccf5a1077dd0d6"
+        },
+        "date": 1735930041004,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8271,
+            "unit": "ns",
+            "range": 0.820673
+          },
+          {
+            "name": "No logging",
+            "value": 2906.81,
+            "unit": "ns",
+            "range": 55.2258
+          },
+          {
+            "name": "Zipkin",
+            "value": 4897.83,
+            "unit": "ns",
+            "range": 199.361
+          },
+          {
+            "name": "Prometheus",
+            "value": 4443.25,
+            "unit": "ns",
+            "range": 109.147
           }
         ]
       }
