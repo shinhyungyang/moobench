cd584fe20f05b6c47801552e7c63da9f1476dd1e
github-action-benchmark <github@users.noreply.github.com>
2025-01-05 04:18:00 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 746bfbe232ddfb39c74e95d1fb47a6f55a90a537

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736050635994,
+  "lastUpdate": 1736050679987,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -77324,6 +77324,52 @@ window.BENCHMARK_DATA = {
             "value": 7957.06,
             "unit": "ns",
             "range": 1017.32
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "746bfbe232ddfb39c74e95d1fb47a6f55a90a537",
+          "message": "Don't source config.rc twice",
+          "timestamp": "2025-01-03T19:07:37Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/746bfbe232ddfb39c74e95d1fb47a6f55a90a537"
+        },
+        "date": 1736050677061,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.573,
+            "unit": "ns",
+            "range": 0.241464
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 331.378,
+            "unit": "ns",
+            "range": 18.4193
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8619.98,
+            "unit": "ns",
+            "range": 1089.85
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8277.48,
+            "unit": "ns",
+            "range": 1042.88
           }
         ]
       }
