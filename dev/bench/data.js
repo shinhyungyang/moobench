cb96881a005a223057f767e97e7b54fbf5022703
github-action-benchmark <github@users.noreply.github.com>
2025-01-05 04:17:16 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for 746bfbe232ddfb39c74e95d1fb47a6f55a90a537

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736050593773,
+  "lastUpdate": 1736050635994,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -28760,6 +28760,52 @@ window.BENCHMARK_DATA = {
             "value": 4276.2,
             "unit": "ns",
             "range": 142.388
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "746bfbe232ddfb39c74e95d1fb47a6f55a90a537",
+          "message": "Don't source config.rc twice",
+          "timestamp": "2025-01-03T19:07:37Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/746bfbe232ddfb39c74e95d1fb47a6f55a90a537"
+        },
+        "date": 1736050632847,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.6745,
+            "unit": "ns",
+            "range": 0.137029
+          },
+          {
+            "name": "No logging",
+            "value": 2853.71,
+            "unit": "ns",
+            "range": 86.431
+          },
+          {
+            "name": "Zipkin",
+            "value": 4731.51,
+            "unit": "ns",
+            "range": 138.665
+          },
+          {
+            "name": "Prometheus",
+            "value": 4377.41,
+            "unit": "ns",
+            "range": 116.158
           }
         ]
       }
