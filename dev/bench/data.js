e8da8386c1fccc7d99e4e695f5f9430e8bf65ba2
github-action-benchmark <github@users.noreply.github.com>
2025-01-09 16:05:39 +0000

    add Kieker-java-2 (customSmallerIsBetter) benchmark result for 8c4a116f9312d9ad75ab00fe7f94eda39b5ef7f9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736438455159,
+  "lastUpdate": 1736438739088,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -44796,6 +44796,54 @@ window.BENCHMARK_DATA = {
             "value": 6253.53,
             "unit": "ns",
             "range": 683.661
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "8c4a116f9312d9ad75ab00fe7f94eda39b5ef7f9",
+          "message": "Also remove verbose for kafka and pinot",
+          "timestamp": "2025-01-09T15:54:05+01:00",
+          "tree_id": "bd27f70aa89d5c5901b0e3c3cd38cd6385099958",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/8c4a116f9312d9ad75ab00fe7f94eda39b5ef7f9"
+        },
+        "date": 1736438735040,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.2946,
+            "unit": "ns",
+            "range": 0.851406
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 565.471,
+            "unit": "ns",
+            "range": 12.7115
+          },
+          {
+            "name": "No logging",
+            "value": 2557.32,
+            "unit": "ns",
+            "range": 61.3847
+          },
+          {
+            "name": "Binary file",
+            "value": 5963.93,
+            "unit": "ns",
+            "range": 762.442
           }
         ]
       }
