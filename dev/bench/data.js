913b027276acf66fc2f723d4bf914f18c90f0a35
github-action-benchmark <github@users.noreply.github.com>
2025-01-10 12:08:16 +0000

    add Kieker-java-8 (customSmallerIsBetter) benchmark result for 226a1959cb5f303c0d15761f0ff240a694247fc4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736510358989,
+  "lastUpdate": 1736510896764,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -76088,6 +76088,54 @@ window.BENCHMARK_DATA = {
             "value": 67608.5,
             "unit": "ns",
             "range": 3191.98
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "226a1959cb5f303c0d15761f0ff240a694247fc4",
+          "message": "Also update benchmark scripts",
+          "timestamp": "2025-01-10T11:21:07+01:00",
+          "tree_id": "5d6b9687b796849cca6788bd7ba557ce12411ea2",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/226a1959cb5f303c0d15761f0ff240a694247fc4"
+        },
+        "date": 1736510892453,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 180.521,
+            "unit": "ns",
+            "range": 6.98435
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 1650.21,
+            "unit": "ns",
+            "range": 52.9934
+          },
+          {
+            "name": "No logging",
+            "value": 6570.46,
+            "unit": "ns",
+            "range": 319.572
+          },
+          {
+            "name": "Binary file",
+            "value": 54328.5,
+            "unit": "ns",
+            "range": 2399.03
           }
         ]
       }
