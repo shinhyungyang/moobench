1c42c1d6f5341f2a3bbd4544436c09ecadc3b77f
github-action-benchmark <github@users.noreply.github.com>
2025-02-13 04:15:16 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 828ad74abbf648d2d943066a220fce1156f810f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739420063516,
+  "lastUpdate": 1739420116328,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -94116,6 +94116,52 @@ window.BENCHMARK_DATA = {
             "value": 8737.7,
             "unit": "ns",
             "range": 1008.56
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "828ad74abbf648d2d943066a220fce1156f810f6",
+          "message": "Define MOOBENCH_CONFIGURATIONS for getGHActionsFormat.sh",
+          "timestamp": "2025-01-22T20:31:38Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/828ad74abbf648d2d943066a220fce1156f810f6"
+        },
+        "date": 1739420110883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.0912,
+            "unit": "ns",
+            "range": 0.207228
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 333.62,
+            "unit": "ns",
+            "range": 24.4562
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8111.34,
+            "unit": "ns",
+            "range": 1132.15
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8068.99,
+            "unit": "ns",
+            "range": 1151.28
           }
         ]
       }
