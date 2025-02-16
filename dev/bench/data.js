c25428d674a001a710faeea1fb4ad42d05537292
github-action-benchmark <github@users.noreply.github.com>
2025-02-16 05:29:12 +0000

    add Pinpoint-java (customSmallerIsBetter) benchmark result for 828ad74abbf648d2d943066a220fce1156f810f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739682913269,
+  "lastUpdate": 1739683752417,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -101802,6 +101802,52 @@ window.BENCHMARK_DATA = {
             "value": 6710.67,
             "unit": "ns",
             "range": 241.12
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "828ad74abbf648d2d943066a220fce1156f810f6",
+          "message": "Define MOOBENCH_CONFIGURATIONS for getGHActionsFormat.sh",
+          "timestamp": "2025-01-22T20:31:38Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/828ad74abbf648d2d943066a220fce1156f810f6"
+        },
+        "date": 1739683747567,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.0431,
+            "unit": "ns",
+            "range": 0.199877
+          },
+          {
+            "name": "Pinpoint Deactivated",
+            "value": 94.7924,
+            "unit": "ns",
+            "range": 2.46222
+          },
+          {
+            "name": "Pinpoint Basic",
+            "value": 11960,
+            "unit": "ns",
+            "range": 666.508
+          },
+          {
+            "name": "Pinpoint Sampling",
+            "value": 6843.83,
+            "unit": "ns",
+            "range": 275.13
           }
         ]
       }
