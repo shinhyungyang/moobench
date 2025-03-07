9444097be6b036654189d693b0c181ae170c07ee
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 04:17:52 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741268163120,
+  "lastUpdate": 1741321071946,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -20682,6 +20682,52 @@ window.BENCHMARK_DATA = {
             "value": 3878.86,
             "unit": "ns",
             "range": 83.3888
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741321066657,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.1469,
+            "unit": "ns",
+            "range": 0.219221
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 443.211,
+            "unit": "ns",
+            "range": 5.16387
+          },
+          {
+            "name": "No logging",
+            "value": 1866.2,
+            "unit": "ns",
+            "range": 6.87397
+          },
+          {
+            "name": "Binary file",
+            "value": 3886.94,
+            "unit": "ns",
+            "range": 60.5817
           }
         ]
       }
