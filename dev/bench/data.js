190ab7c340242a41910b8536b0685c42f53dbd8e
github-action-benchmark <github@users.noreply.github.com>
2025-03-09 05:08:25 +0000

    add inspectIT-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741493418038,
+  "lastUpdate": 1741496905694,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -105774,6 +105774,58 @@ window.BENCHMARK_DATA = {
             "value": 9556.21,
             "unit": "ns",
             "range": 240.294
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741496900264,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.0656,
+            "unit": "ns",
+            "range": 0.196856
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 5598.53,
+            "unit": "ns",
+            "range": 254.326
+          },
+          {
+            "name": "No logging",
+            "value": 15688.8,
+            "unit": "ns",
+            "range": 469.315
+          },
+          {
+            "name": "Zipkin",
+            "value": 17011.3,
+            "unit": "ns",
+            "range": 281.869
+          },
+          {
+            "name": "Prometheus",
+            "value": 9546.52,
+            "unit": "ns",
+            "range": 228.362
           }
         ]
       }
