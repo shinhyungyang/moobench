f457a0cbb80737ce031612222f03934cadc41d30
github-action-benchmark <github@users.noreply.github.com>
2025-03-10 03:51:41 +0000

    add Scouter-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741497919184,
+  "lastUpdate": 1741578701155,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -109076,6 +109076,46 @@ window.BENCHMARK_DATA = {
             "value": 22621.1,
             "unit": "ns",
             "range": 95.5742
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741578695135,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.1014,
+            "unit": "ns",
+            "range": 0.231713
+          },
+          {
+            "name": "Instrumentation and Profiling",
+            "value": 25264.6,
+            "unit": "ns",
+            "range": 258.432
+          },
+          {
+            "name": "Disabled Profiling",
+            "value": 22062.2,
+            "unit": "ns",
+            "range": 233.793
           }
         ]
       }
