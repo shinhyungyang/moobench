2e362c593fc4e9b455174d77b9d6af3360c7834f
github-action-benchmark <github@users.noreply.github.com>
2025-03-10 04:38:24 +0000

    add Kieker-java-8 (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741580147510,
+  "lastUpdate": 1741581504806,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -87500,6 +87500,52 @@ window.BENCHMARK_DATA = {
             "value": 54760.3,
             "unit": "ns",
             "range": 1306.25
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741581499154,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 182.085,
+            "unit": "ns",
+            "range": 3.86211
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 1583.19,
+            "unit": "ns",
+            "range": 64.5643
+          },
+          {
+            "name": "No logging",
+            "value": 6538.06,
+            "unit": "ns",
+            "range": 381.181
+          },
+          {
+            "name": "Binary file",
+            "value": 53602.1,
+            "unit": "ns",
+            "range": 1497.53
           }
         ]
       }
