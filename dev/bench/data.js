61af5983b029b34d3e268c3745bb48619bad7f1c
github-action-benchmark <github@users.noreply.github.com>
2025-03-11 04:18:27 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741583433567,
+  "lastUpdate": 1741666707807,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -20866,6 +20866,52 @@ window.BENCHMARK_DATA = {
             "value": 3969.28,
             "unit": "ns",
             "range": 67.8145
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741666702293,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 91.9699,
+            "unit": "ns",
+            "range": 0.199983
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 443.106,
+            "unit": "ns",
+            "range": 6.58951
+          },
+          {
+            "name": "No logging",
+            "value": 1864.61,
+            "unit": "ns",
+            "range": 7.12552
+          },
+          {
+            "name": "Binary file",
+            "value": 3966.25,
+            "unit": "ns",
+            "range": 97.8483
           }
         ]
       }
