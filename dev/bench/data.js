7d3bd3354f1f3e3c34a3b4dc0c1b26978d1f3b98
github-action-benchmark <github@users.noreply.github.com>
2025-03-11 04:22:33 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741666905586,
+  "lastUpdate": 1741666953259,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -99840,6 +99840,52 @@ window.BENCHMARK_DATA = {
             "value": 8040.15,
             "unit": "ns",
             "range": 1027.6
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1741666947567,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.1823,
+            "unit": "ns",
+            "range": 0.144433
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 334.03,
+            "unit": "ns",
+            "range": 27.79
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8465.87,
+            "unit": "ns",
+            "range": 1144.81
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8009.62,
+            "unit": "ns",
+            "range": 902.459
           }
         ]
       }
