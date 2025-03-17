4a621bc8b9c062290d8ac3c798dbd1a8f37b8e65
github-action-benchmark <github@users.noreply.github.com>
2025-03-17 05:40:15 +0000

    add Pinpoint-java (customSmallerIsBetter) benchmark result for 6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742188934921,
+  "lastUpdate": 1742190015056,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -110548,6 +110548,52 @@ window.BENCHMARK_DATA = {
             "value": 6739.14,
             "unit": "ns",
             "range": 266.713
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a",
+          "message": "Merge pull request #25 from Marcel-Ha/support-skywalking\n\nAdd benchmark action for skywalking",
+          "timestamp": "2025-03-06T11:42:45Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b1233d975b6fe4a813fd4c377c0da7fbbfeeb2a"
+        },
+        "date": 1742190009237,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.2702,
+            "unit": "ns",
+            "range": 0.370251
+          },
+          {
+            "name": "Pinpoint Deactivated",
+            "value": 96.0357,
+            "unit": "ns",
+            "range": 7.31419
+          },
+          {
+            "name": "Pinpoint Basic",
+            "value": 11956.2,
+            "unit": "ns",
+            "range": 605.027
+          },
+          {
+            "name": "Pinpoint Sampling",
+            "value": 6708.23,
+            "unit": "ns",
+            "range": 164.866
           }
         ]
       }
