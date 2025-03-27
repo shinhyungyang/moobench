dd66e46a2a2f755adc4b7958c047d328ac6dd79f
github-action-benchmark <github@users.noreply.github.com>
2025-03-27 05:42:40 +0000

    add Pinpoint-java (customSmallerIsBetter) benchmark result for 89b2c7d35c370b743d84c7bf258a7ec5b4f410ca

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743053035467,
+  "lastUpdate": 1743054160333,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -114056,6 +114056,52 @@ window.BENCHMARK_DATA = {
             "value": 6608.44,
             "unit": "ns",
             "range": 193.531
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "89b2c7d35c370b743d84c7bf258a7ec5b4f410ca",
+          "message": "More debut output",
+          "timestamp": "2025-03-23T10:35:43Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/89b2c7d35c370b743d84c7bf258a7ec5b4f410ca"
+        },
+        "date": 1743054153836,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.0209,
+            "unit": "ns",
+            "range": 0.148725
+          },
+          {
+            "name": "Pinpoint Deactivated",
+            "value": 93.6602,
+            "unit": "ns",
+            "range": 2.11634
+          },
+          {
+            "name": "Pinpoint Basic",
+            "value": 12137.8,
+            "unit": "ns",
+            "range": 442.164
+          },
+          {
+            "name": "Pinpoint Sampling",
+            "value": 6881.77,
+            "unit": "ns",
+            "range": 382.661
           }
         ]
       }
