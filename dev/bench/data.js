7e271fce140ccee2258c6521564972201c3a025d
github-action-benchmark <github@users.noreply.github.com>
2025-03-29 04:23:49 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 89b2c7d35c370b743d84c7bf258a7ec5b4f410ca

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743221974141,
+  "lastUpdate": 1743222229266,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -103418,6 +103418,52 @@ window.BENCHMARK_DATA = {
             "value": 8069.75,
             "unit": "ns",
             "range": 1049.69
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "89b2c7d35c370b743d84c7bf258a7ec5b4f410ca",
+          "message": "More debut output",
+          "timestamp": "2025-03-23T10:35:43Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/89b2c7d35c370b743d84c7bf258a7ec5b4f410ca"
+        },
+        "date": 1743222222645,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.1317,
+            "unit": "ns",
+            "range": 0.170936
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 338.917,
+            "unit": "ns",
+            "range": 51.2597
+          },
+          {
+            "name": "Regular Writing",
+            "value": 7826.93,
+            "unit": "ns",
+            "range": 956.675
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8064.73,
+            "unit": "ns",
+            "range": 967.969
           }
         ]
       }
