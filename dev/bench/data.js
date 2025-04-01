e30f47af834584b26072c49756ce5df5a5a263c5
github-action-benchmark <github@users.noreply.github.com>
2025-04-01 05:35:12 +0000

    add inspectIT-java (customSmallerIsBetter) benchmark result for 89b2c7d35c370b743d84c7bf258a7ec5b4f410ca

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743482238673,
+  "lastUpdate": 1743485711971,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -111884,6 +111884,58 @@ window.BENCHMARK_DATA = {
             "value": 9748.37,
             "unit": "ns",
             "range": 284.98
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "89b2c7d35c370b743d84c7bf258a7ec5b4f410ca",
+          "message": "More debut output",
+          "timestamp": "2025-03-23T10:35:43Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/89b2c7d35c370b743d84c7bf258a7ec5b4f410ca"
+        },
+        "date": 1743485705470,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.1073,
+            "unit": "ns",
+            "range": 0.238941
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 5542.76,
+            "unit": "ns",
+            "range": 272.041
+          },
+          {
+            "name": "No logging",
+            "value": 15790.2,
+            "unit": "ns",
+            "range": 283.049
+          },
+          {
+            "name": "Zipkin",
+            "value": 17034.8,
+            "unit": "ns",
+            "range": 154.493
+          },
+          {
+            "name": "Prometheus",
+            "value": 9636.71,
+            "unit": "ns",
+            "range": 252.746
           }
         ]
       }
