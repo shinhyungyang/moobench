b128f7461efad1a12ab50d034d4f5cca27b2da79
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 04:34:00 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746160150803,
+  "lastUpdate": 1746160440073,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -108944,6 +108944,52 @@ window.BENCHMARK_DATA = {
             "value": 8687.77,
             "unit": "ns",
             "range": 1098.65
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1746160432681,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 91.1318,
+            "unit": "ns",
+            "range": 0.423327
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 319.047,
+            "unit": "ns",
+            "range": 26.9493
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8117.04,
+            "unit": "ns",
+            "range": 1096.08
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8059,
+            "unit": "ns",
+            "range": 533.35
           }
         ]
       }
