1d57ba8f41b057f358c79ac4dab24b4d3fe78e0a
github-action-benchmark <github@users.noreply.github.com>
2025-05-05 04:36:26 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746419723897,
+  "lastUpdate": 1746419786063,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -109450,6 +109450,52 @@ window.BENCHMARK_DATA = {
             "value": 9144.13,
             "unit": "ns",
             "range": 890.376
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1746419778506,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9437,
+            "unit": "ns",
+            "range": 0.0707552
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 339.233,
+            "unit": "ns",
+            "range": 18.0377
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8134.92,
+            "unit": "ns",
+            "range": 1133.72
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8399,
+            "unit": "ns",
+            "range": 929.696
           }
         ]
       }
