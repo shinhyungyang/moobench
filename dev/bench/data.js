a471f741833606e92324baeb3384a9337e5ee0b9
github-action-benchmark <github@users.noreply.github.com>
2025-05-12 04:18:33 +0000

    add Scouter-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747023406239,
+  "lastUpdate": 1747023513319,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -125698,6 +125698,46 @@ window.BENCHMARK_DATA = {
             "value": 22261.6,
             "unit": "ns",
             "range": 265.414
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1747023505542,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.851,
+            "unit": "ns",
+            "range": 0.0848916
+          },
+          {
+            "name": "Instrumentation and Profiling",
+            "value": 25187.9,
+            "unit": "ns",
+            "range": 261.753
+          },
+          {
+            "name": "Disabled Profiling",
+            "value": 21993.1,
+            "unit": "ns",
+            "range": 364.086
           }
         ]
       }
