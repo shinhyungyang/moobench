8a8aa39191e27b94ead646f041e987e8086e4994
github-action-benchmark <github@users.noreply.github.com>
2025-05-22 04:28:27 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747806764713,
-  "repoUrl": "https://github.com/kieker-monitoring/moobench",
+  "lastUpdate": 1747888107112,
+  "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
       {
@@ -24514,6 +24514,52 @@ window.BENCHMARK_DATA = {
             "value": 3851.65,
             "unit": "ns",
             "range": 86.0372
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1747888099178,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8823,
+            "unit": "ns",
+            "range": 0.135789
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 439.101,
+            "unit": "ns",
+            "range": 3.94501
+          },
+          {
+            "name": "No logging",
+            "value": 1840.64,
+            "unit": "ns",
+            "range": 6.16867
+          },
+          {
+            "name": "Binary file",
+            "value": 3875.93,
+            "unit": "ns",
+            "range": 66.9293
           }
         ]
       }
