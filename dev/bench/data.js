ac24066dc645b2178798d23470c75fd35463c219
github-action-benchmark <github@users.noreply.github.com>
2025-05-25 04:47:14 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748148227230,
+  "lastUpdate": 1748148434332,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -112716,6 +112716,52 @@ window.BENCHMARK_DATA = {
             "value": 7684.24,
             "unit": "ns",
             "range": 756.985
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1748148425697,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8793,
+            "unit": "ns",
+            "range": 0.0944094
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 333.008,
+            "unit": "ns",
+            "range": 10.9999
+          },
+          {
+            "name": "Regular Writing",
+            "value": 7989.59,
+            "unit": "ns",
+            "range": 1108.75
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8360.05,
+            "unit": "ns",
+            "range": 900.095
           }
         ]
       }
