ad4e8d4ff4dab6a7c01e13dab719e47ae84d666e
github-action-benchmark <github@users.noreply.github.com>
2025-06-13 08:37:22 +0000

    add Kieker-java-12 (customSmallerIsBetter) benchmark result for 51267c63ebabbfe92506133e25657b577360bd94

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749803802922,
+  "lastUpdate": 1749803842234,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -104280,6 +104280,54 @@ window.BENCHMARK_DATA = {
             "value": 115611,
             "unit": "ns",
             "range": 3214.69
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shinhyung.yang@email.uni-kiel.de",
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang"
+          },
+          "committer": {
+            "email": "shinhyung.yang@email.uni-kiel.de",
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang"
+          },
+          "distinct": true,
+          "id": "51267c63ebabbfe92506133e25657b577360bd94",
+          "message": "GitHub action triggers for the daily branch",
+          "timestamp": "2025-06-13T08:40:55+02:00",
+          "tree_id": "d781c2ff7588256153903b834c99f6897f866f17",
+          "url": "https://github.com/shinhyungyang/moobench/commit/51267c63ebabbfe92506133e25657b577360bd94"
+        },
+        "date": 1749803832207,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 262.308,
+            "unit": "ns",
+            "range": 11.4903
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 2560.24,
+            "unit": "ns",
+            "range": 129.419
+          },
+          {
+            "name": "No logging",
+            "value": 9581.08,
+            "unit": "ns",
+            "range": 665.338
+          },
+          {
+            "name": "Binary file",
+            "value": 120679,
+            "unit": "ns",
+            "range": 7621.04
           }
         ]
       }
