e333261761d4009b029bb771a3f060650a368702
github-action-benchmark <github@users.noreply.github.com>
2025-07-04 12:00:24 +0200

    add Kieker-java (Binary file, Manjaro, self-hosted) (customSmallerIsBetter) benchmark result for 967e2e0b5e8c5e7ce257e23b922a23331f662518

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751622287560,
+  "lastUpdate": 1751623224853,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java (Binary file, Manjaro, self-hosted)": [
@@ -28,6 +28,34 @@ window.BENCHMARK_DATA = {
             "value": 5861.5,
             "unit": "ns",
             "range": 620.227
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "967e2e0b5e8c5e7ce257e23b922a23331f662518",
+          "message": "Merge branch 'compare-runners'",
+          "timestamp": "2025-07-02T13:01:38Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/967e2e0b5e8c5e7ce257e23b922a23331f662518"
+        },
+        "date": 1751623224018,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "6.12.34-1-MANJARO on self-hosted Runner (250109)",
+            "value": 5285.84,
+            "unit": "ns",
+            "range": 450.86
           }
         ]
       }
