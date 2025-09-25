5538e2c96aacc3da27236a5157b13779a5fda08b
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-09-25 09:08:02 +0200

    Removed cpu check for the Prometheus configuration

--- frameworks/OpenTelemetry-java/labels.sh
+++ frameworks/OpenTelemetry-java/labels.sh
@@ -2,9 +2,9 @@ TITLE[0]="No instrumentation"
 TITLE[1]="No logging"
 TITLE[2]="Logging"
 TITLE[3]="Zipkin"
+TITLE[4]="Prometheus"
 MACHINE_TYPE=`uname -m`; 
 if [ ${MACHINE_TYPE} == 'x86_64' ]
 then
-	TITLE[4]="Prometheus"
 	#TITLE[5]="OpenTelemetry Jaeger"
 fi
