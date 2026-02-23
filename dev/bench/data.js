8bd62c23ad6b96c5ad584bd0aa9293ae13746cd6
github-action-benchmark <github@users.noreply.github.com>
2026-02-23 06:04:57 +0000

    add Kieker-java-8 (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771826535270,
+  "lastUpdate": 1771826697240,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -128809,6 +128809,52 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 13019.6,
             "range": "13030.5",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1771826641506,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 92.4942,
+            "range": "92.6867",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 120.444,
+            "range": "120.661",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 1586.41,
+            "range": "1586.85",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 13259.9,
+            "range": "13261.4",
             "unit": "ns"
           }
         ]
