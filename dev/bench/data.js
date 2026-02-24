074d9e8dd003489a5b95b19e9a1e494612852815
github-action-benchmark <github@users.noreply.github.com>
2026-02-24 06:22:12 +0000

    add Kieker-java-12 (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771912533388,
+  "lastUpdate": 1771914131872,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -141089,6 +141089,52 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 29174.3,
             "range": "29209.2",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1771914061946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 122.697,
+            "range": "123.657",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 175.265,
+            "range": "176.007",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 2663.73,
+            "range": "2664.91",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 25384.4,
+            "range": "25404",
             "unit": "ns"
           }
         ]
