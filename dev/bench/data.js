cdc089bcc517a01e55e598250b5beb1bf539f3b9
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 06:09:16 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774767611127,
+  "lastUpdate": 1774850956771,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -39171,6 +39171,52 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 1010.97,
             "range": "1011.43",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1774850897975,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.9277,
+            "range": "44.928",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 49.5576,
+            "range": "49.5665",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 468.237,
+            "range": "468.25",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 1020.22,
+            "range": "1020.62",
             "unit": "ns"
           }
         ]
