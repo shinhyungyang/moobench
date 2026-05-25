6989426abd8ceb0fc07fec641320f69ef96b3aa4
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 07:56:50 +0000

    add Scouter-java (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779611099896,
+  "lastUpdate": 1779695810215,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -221667,6 +221667,46 @@ window.BENCHMARK_DATA = {
             "name": "Disabled Profiling",
             "value": 9944.3,
             "range": "9946.13",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1779695743307,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.0156,
+            "range": "45.0158",
+            "unit": "ns"
+          },
+          {
+            "name": "Instrumentation and Profiling",
+            "value": 12868.5,
+            "range": "12873.3",
+            "unit": "ns"
+          },
+          {
+            "name": "Disabled Profiling",
+            "value": 11083.7,
+            "range": "11084.8",
             "unit": "ns"
           }
         ]
