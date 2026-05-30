73ef8af8bf0b43fadd7af313b03ddd22d2cdf396
github-action-benchmark <github@users.noreply.github.com>
2026-05-30 08:11:36 +0000

    add inspectIT-java (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780126959461,
+  "lastUpdate": 1780128696012,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -211721,6 +211721,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 2001.86,
             "range": "2002.26",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1780128628717,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.0648,
+            "range": "45.065",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 814.674,
+            "range": "816.23",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 4888.26,
+            "range": "4889.74",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5182.99,
+            "range": "5185.02",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 1996.08,
+            "range": "1997.65",
             "unit": "ns"
           }
         ]
