17e5d1c1d9fbd90fe9a599942f8d5e35e307c9ae
github-action-benchmark <github@users.noreply.github.com>
2026-09-07 08:39:05 +0000

    add Kieker-java-4 (customSmallerIsBetter) benchmark result for f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788770216701,
+  "lastUpdate": 1788770344904,
   "repoUrl": "https://github.com/shinhyungyang/moobench",
   "entries": {
     "Kieker-java": [
@@ -129971,6 +129971,52 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 2800.35,
             "range": "2809.07",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "id": "f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54",
+          "message": "Merge remote-tracking branch 'upstream'",
+          "timestamp": "2025-09-24T06:51:03Z",
+          "url": "https://github.com/shinhyungyang/moobench/commit/f7b90914d9328e1f8d0b1b9c6e40195bdbac5f54"
+        },
+        "date": 1788770276316,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 52.8507,
+            "range": "52.8546",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 64.3648,
+            "range": "64.3751",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 805.418,
+            "range": "806.097",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 2839.72,
+            "range": "2843.04",
             "unit": "ns"
           }
         ]
